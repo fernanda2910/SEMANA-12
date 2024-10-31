0dcd36cd27297d9b436da97b29b9246b7085ecff
fernanda2910 <blxckfxr@gmail.com>
2024-10-31 12:19:07 -0500

    Primer commit

---
+++ Estudiantes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class Estudiantes
+    {
+        Byte[] edades = new byte[0];
+        int posicion = 0;
+        public void insertar()
+        {
+            String seguir;
+            byte ed;
+            do
+            {
+                Console.WriteLine("\nINGRESE EDAD: ");
+                while (!byte.TryParse(Console.ReadLine(), out ed) || ed < 14 || ed > 120)
+                {
+                    Console.Write("ERROR: INGRESAR EDAD: ");
+                }
+                Array.Resize(ref edades, edades.Length + 1);
+                edades[posicion] = ed;
+                posicion++;
+                Console.WriteLine("\nEDAD REGISTRADA CORRECTAMENTE");
+                Console.Write("\nDESEA SEGUIR REGISTRANDO? [SI]:");
+                seguir = Console.ReadLine().ToUpper();
+            } while (seguir == "SI");
+        }
+        public void mostrar()
+        {
+            Console.SetCursorPosition(10, 10);
+            Console.Write("POSICION");
+            Console.SetCursorPosition(30, 10);
+            Console.Write("EDADES");
+            for (int i = 0; i < edades.Length; i++)
+            {
+                Console.SetCursorPosition(10, 11 + i);
+                Console.Write(i);
+                Console.SetCursorPosition(30, 11 + i);
+                Console.Write(edades[i]);
+            }
+        }
+        public void eliminar (byte ed)
+        {
+            int indice = Array.IndexOf(edades, ed); 
+            if (indice != -1)
+            {
+                for (int i = indice; i < edades.Length -1; i++)
+                {
+                    edades[i] = edades[i + 1];
+                }
+                Array.Resize(ref edades, edades.Length - 1);
+                posicion--;
+                Console.WriteLine("EDAD ELIMINADA CORRECTAMENTE: ");
+            }
+            else
+            {
+                Console.WriteLine("\nNO SE PUEDE ELIMINAR PORQUE NO EXISTE");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp13;
+using System.Runtime.InteropServices;
+
+byte opcion;
+String regresar;
+Estudiantes e = new Estudiantes();
+do
+{
+    Console.WriteLine("BIENVENIDOS AL SITIO DE REGISTRO DE EDADES");
+    Console.WriteLine("******** MENU *********");
+    Console.WriteLine("     1. INSERTAR     ");
+    Console.WriteLine("     2. MOSTRAR     ");
+    Console.WriteLine("     3. ELIMINAR     ");
+    Console.WriteLine("     4. ORDENAR     ");
+    Console.WriteLine("     0. SALIR     ");
+    Console.WriteLine("***********************");
+    Console.Write("INGRESE OPCION: ");
+    while (!byte.TryParse(Console.ReadLine(), out opcion) || opcion > 4)
+    {
+        Console.WriteLine("ERROR: INGRESE OPCION: ");
+    }
+    switch (opcion)
+    {
+        case 0: Environment.Exit(0); break;
+        case 1: e.insertar(); break;
+        case 2: e.mostrar(); break;
+        case 3:
+            Console.Write("\nINGRESE EDAD A ELIMINAR: ");
+            byte ed = byte.Parse(Console.ReadLine());
+            e.eliminar(ed);
+            break;
+        case 4: break;
+    }
+    Console.WriteLine("\n\nDESEA REGRESAR AL MENU? [SI]");
+    regresar = Console.ReadLine().ToUpper();
+    Console.Clear();
+} while (regresar == "SI");
